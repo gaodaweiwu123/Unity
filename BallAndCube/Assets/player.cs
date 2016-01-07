39a8fd30d68721dc230a0f13d1bdccd4db962ab2
gaodaweiwu123 <1325953748@qq.com>
2016-01-07 20:46:58 +0800

    first_commit

---
+++ BallAndCube/Assets/player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+    protected float jump_speed = 12.0f;
+    public bool is_landing = false;
+	// Update is called once per frame
+	void Update () {
+        
+	    if(this.is_landing){
+            if (Input.GetMouseButtonDown(0))
+            {
+                this.GetComponent<Rigidbody>().velocity = Vector3.up * this.jump_speed;
+            }
+        }
+	}
+    void Start()
+    {
+        this.is_landing = false;
+    }
+    void OnCollisionEnter(Collision collision)
+     {
+        if (collision.gameObject.tag =="Floor") {
+        this.is_landing = true;
+        }
+        
+     }
+    
+}
